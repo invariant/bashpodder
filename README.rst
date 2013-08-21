114aff7ff459db2acc50a654669e8725f0c9ad61
Trevor L. Davis <trevor.l.davis@stanford.edu>
2013-08-20 20:01:51 -0700

    Personalize bashpodder

---
+++ README.rst
@@ -0,0 +1,64 @@
+~~~~~~~~~~
+Bashpodder
+~~~~~~~~~~
+
+This is my modified version of the bashpodder podcatcher script. It is available under the GPL.
+
+The original is available at https://github.com/lincgeek/bashpodder .
+This includes enhancements made by Ellen Taylor available at https://github.com/ellencubed/bashpodder .
+
+Filenames from popupchinese.com podcasts are automatically cleaned up before downloading.
+
+Usage
+~~~~~
+
+This iteration assumes that bashpodder.bash, bp.conf, parse_enclosure.xsl, and podcast.log are in the directory you wish to download podcasts to.
+
+To print out (and store in test.log) what files bashpodder would download without actually downloading them::
+  
+  bash$ bash bashpodder.bash test
+
+To download any podcast files not found in podcast.log::
+  
+  bash$ bash bashpodder.bash download
+
+Like the original bashpodder and unlike Ellen Taylor's version which downloads only the last podcast 
+the download action will download ALL available podcasts not recorded in pdocast.log.
+However the test action shows you all the files it will download so you can place undesired ones the into podcast.log 
+before actually downloading.  To mark ALL podcasts as downloaded (so in the future you'll only download new podcasts) 
+you can do::
+
+    bash$ bashpodder.bash test >> podcast.log
+
+You can sometimes place previously downloaded files from other podcatchers into podcast.log using the ``ls`` command::
+
+    bash$ ls PopupChinese/*.mp3 >> podcast.log
+
+Files downloaded by bashpodder will automatically be recorded into podcast.log.  
+To re-download simply delete the entry and run bashpodder.
+
+Bashpodder requires you have bash, wget, awk, sed and xsltproc.
+
+Config
+~~~~~~
+
+Feeds are defined in bp.conf as::
+
+  http://podcastfeedurl.com/rss feeddir
+
+'feeddir' being the subdirectory for that particular feed. 
+
+bp.conf.example contains an example with some of my favorite non-personalized Podcasts (to use it rename it to bp.conf).
+
+The podcast directory to download files is defined by the datadir environmental variable in bashpodder.bash and defaults to the working directory.
+
+Create playlists
+----------------
+You can create simple playlists by listing files in a text files with m3u suffix::
+
+    bash$ ls PopupChinese/*dialogue.mp3 > PopupChinese/dialogue_playlist.m3u
+
+Schedule jobs
+-------------
+
+To regularly download scripts you can set up a crontab job.
---
+++ bashpodder.bash
@@ -0,0 +1,107 @@
+#!/bin/bash
+# By Linc 10/1/2004
+# Find the latest script at http://lincgeek.org/bashpodder
+# Revision 1.21 12/04/2008 - Many Contributers!
+# If you use this and have made improvements or have comments
+# drop me an email at linc dot fessenden at gmail dot com
+# and post your changes to the forum at http://lincgeek.org/lincware
+# I'd appreciate it!
+
+# modified by ellen taylor (ellencubed at gmail dot com)
+# modified by trevor davis (trevor.l.davis at gmail dot com) 2013
+
+# datadir is the directory you want podcasts saved to:
+# datadir=$HOME/Documents/Podcasts
+datadir=$(pwd)
+
+function main {
+    case $1 in
+        "download")
+            download temp.log true
+            # Move dynamically created log file to permanent log file:
+            cat podcast.log >> temp.log
+            sort temp.log | uniq > podcast.log
+            rm temp.log
+            # Create an m3u playlist:
+            #ls $datadir | grep -v m3u > $datadir/podcast.m3u
+            ;;
+        "test")
+            download test.log false
+            cat test.log
+            ;;
+        "help" | "usage" | *)
+            usage;;
+    esac
+}
+
+function usage {
+    echo "Usage: bashpodder.bash download | test | help"
+}
+
+function clean_popup {
+    url=$1
+    filetype=$(echo $url | sed -e 's/.*\(mp3\|pdf\)$/\1/' )
+    lesson=$(echo $(basename $url) | sed -e 's/\(.*\).\(pdf\|mp3\)%.*/\1/' )
+    # lesson=$(echo $url | cut -d/ -f7)
+    level=$(echo $url | cut -d/ -f6)
+    mediatype=$(echo $url | cut -d/ -f5)
+    if [ $mediatype = $filetype ] ; then
+        echo "popupchinese-${level}-${lesson}.$filetype"
+    else
+        echo "popupchinese-${level}-${lesson}-${mediatype}.$filetype"
+    fi
+}
+
+function produce_filename {
+    url=$1
+    if [[ $url =~ "popupchinese.com" ]] ; then
+        filename=$(clean_popup $url)
+    else
+        #filename=$(echo $url | awk -F'/' '{print $NF}')
+        filename=$(echo "$url" | awk -F'/' {'print $NF'} | awk -F'=' {'print $NF'} | awk -F'?' {'print $1'})
+    fi
+    echo $filename
+}
+
+function download {
+    logfile=$1
+    download_media=$2
+
+    # Make script crontab friendly:
+    cd $(dirname $0)
+
+    # create datadir if necessary:
+    if $download_media ; then
+        mkdir -p $datadir
+    fi
+    touch podcast.log
+
+    # Delete any temp file:
+    rm -f $logfile
+
+    # Create new playlist
+    # echo "#Last fetch on $(date +%Y-%m-%d) @ $(date +%r)" > $datadir/latest.m3u
+
+    # Read the bp.conf file and wget any file not already in the podcast.log file (if ``download_media`` is true):
+    while read podcastfields
+            do
+            podcast=$(echo $podcastfields | cut -d' ' -f1)
+            dname=$(echo $podcastfields | cut -d' ' -f2)
+            file=$(xsltproc parse_enclosure.xsl $podcast 2> /dev/null || wget -q $podcast -O - | tr '\r' '\n' | tr \' \" | sed -n 's/.*url="\([^"]*\)".*/\1/p')
+            mkdir -p $datadir/$dname
+            for url in $file
+                    do
+                    filename=$(produce_filename $url)
+                    if ! grep "$filename" podcast.log > /dev/null ; then
+                        echo $filename >> $logfile
+                        if $download_media ; then
+                            wget -t 10 -U BashPodder -c -q -O $datadir/$dname/$filename "$url"
+                        fi
+                        #echo $datadir/$dname/$filename >> $datadir/latest.m3u
+                    fi
+                    done
+            done < bp.conf
+
+}
+
+main $@
